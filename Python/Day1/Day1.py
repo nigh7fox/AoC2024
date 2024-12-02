a6f23ffca58eae4ece3a9ea2d2f0eac496d7e0bc
Nigh7fox <joshua.shaldrick.tromp@gmail.com>
2024-12-02 10:52:15 +0100

    Still missing exercise two of both days

--- Python/Day1/Day1.py
+++ Python/Day1/Day1.py
@@ -28,10 +28,35 @@ def findSmallestAndAdd(nums):
     return answer
 
 
-def main():
-    print(findSmallestAndAdd(readInput()))
-    return 0
+def findSmallestAndAddV2(nums):
+    leftHalf = []
+    rightHalf = []
+    answer = 0
+    for i, num in enumerate(nums):
+        splitArr = num.split()
+        leftHalf.append(int(splitArr[0]))
+        rightHalf.append(int(splitArr[1]))
+
+    while len(leftHalf) >= 2:
+        answer = answer + abs((min(rightHalf) - min(leftHalf)))
+        # Logs test
+        # print(f"Size lists left: {len(leftHalf)} right: {len(rightHalf)}")
+        # print(f"Popping left index: {leftHalf.index(min(leftHalf))} right index: {rightHalf.index(min(rightHalf))}")
+        # print(f"Add to answer = {min(rightHalf)} - {min(leftHalf)}")
+        # print(f"Right: {rightHalf} Left: {leftHalf}")
+        if (len(leftHalf) == 2):
+            answer = answer + abs((max(rightHalf) - max(leftHalf)))
+        leftHalf.pop(leftHalf.index(min(leftHalf)))
+        rightHalf.pop(rightHalf.index(min(rightHalf)))
+    return answer
+
+
+def main(part):
+    if part == 1:
+        return findSmallestAndAdd(readInput())
+    return findSmallestAndAddV2(readInput())
 
 
 if __name__ == "__main__":
-    main()
+    print(f"Day 1 exercise 1: {main(1)}")
+    # print(f"Day 1 exercise 2: {main(2)}")
--- Python/Day2/Day2.py
+++ Python/Day2/Day2.py
@@ -5,9 +5,76 @@ def readInput():
     return linesFormatted
 
 
-def main():
-    return 0
+def evalReports(reports):
+    levels = [report.split(" ") for report in reports]
+    intLevels = [list(map(int, lvl)) for lvl in levels]
+    levelsDiff = []
+    levelsAreSafe = []
+    for i, levels in enumerate(intLevels):
+        levelDiff = []
+        for x, level in enumerate(levels):
+            if (x != len(levels) - 1):
+                levelDiff.append((levels[x] - levels[x + 1]))
+        levelsDiff.append(levelDiff)
+    
+    for i, diff in enumerate(levelsDiff):
+        levelIsSafe = 1
+        if(min(diff) == 0):
+            levelIsSafe = 0
+        elif (abs(max(diff)) >= 1 and abs(max(diff) <= 3) and abs(min(diff)) <= 3):
+            levelIsSafe = 1
+        else:
+            levelIsSafe = 0
+
+        intCount = 0
+        for num in diff:
+            if num < 0:
+                intCount = intCount + 1
+        if intCount != len(diff) and intCount != 0:
+            levelIsSafe = 0
+
+        levelsAreSafe.append(levelIsSafe)
+    return sum(levelsAreSafe)
+
+
+def evalReportsV2(reports):
+    levels = [report.split(" ") for report in reports]
+    intLevels = [list(map(int, lvl)) for lvl in levels]
+    levelsDiff = []
+    levelsAreSafe = []
+    for i, levels in enumerate(intLevels):
+        levelDiff = []
+        for x, level in enumerate(levels):
+            if (x != len(levels) - 1):
+                levelDiff.append((levels[x] - levels[x + 1]))
+        levelsDiff.append(levelDiff)
+    
+    for i, diff in enumerate(levelsDiff):
+        levelIsSafe = 1
+        if(min(diff) == 0):
+            levelIsSafe = 0
+        elif (abs(max(diff)) >= 1 and abs(max(diff) <= 3) and abs(min(diff)) <= 3):
+            levelIsSafe = 1
+        else:
+            levelIsSafe = 0
+
+        intCount = 0
+        for num in diff:
+            if num < 0:
+                intCount = intCount + 1
+        if intCount != len(diff) and intCount != 0:
+            levelIsSafe = 0
+
+        levelsAreSafe.append(levelIsSafe)
+    return sum(levelsAreSafe)
+
+
+def main(part):
+    if part == 1:
+        return evalReports(readInput())
+    return evalReportsV2(readInput())
 
 
 if __name__ == "__main__":
-    main()
+    print(f"Day 2 exercise 1: {main(1)}")
+    # print(f"Day 2 exercise 2: {main(2)}")
