b6eb1a51672a144c69dbe41b88cf4a560f173bd1
Nigh7fox <joshua.shaldrick.tromp@gmail.com>
2024-12-05 13:46:10 +0100

    day 5 part 1 prep

--- Python/Day5/Day5.py
+++ Python/Day5/Day5.py
@@ -1,17 +1,46 @@
 def readInput():
-    with open('input.txt') as f:
+    with open('test_input.txt') as f:
         lines = f.readlines()
     linesFormatted = [x.strip('\n') for x in lines]
     return linesFormatted
 
 
-def printQueue():
-    return 0
+def formatPrintQueue():
+    lines = readInput()
+    pageOrderingRules = []
+    printQueue = []
+    for i, line in enumerate(lines):
+        if line == "":
+            print(f"Seperator at: {i}")
+            pageOrderingRules = lines[0:i]
+            printQueue = lines[i+1:len(lines)]
+    return [pageOrderingRules, printQueue]
+
+
+def getPageRules(rules, pageNum):
+    allCurrentPageRules = []
+    for rule in rules:
+        splitRules = str(rule).split("|")
+        for r in splitRules:
+            if int(r) == int(pageNum):
+                allCurrentPageRules.append(rule)
+    return allCurrentPageRules
+
+
+def controlPrintQueue():
+    formattedQueue = formatPrintQueue()
+    rules = formattedQueue[0]
+    queue = formattedQueue[1]
+    for i, pages in enumerate(queue):
+        splitPages = pages.split(",")
+        print(f"Update: {i} - Page numbers: {splitPages}")
+        for page in pages.split(","):
+            print(f"Page: {page} - Rules: {getPageRules(rules, page)}")
 
 
 def main(part):
     if part == 1:
-        return printQueue()
+        return controlPrintQueue()
     return 1
 
 
